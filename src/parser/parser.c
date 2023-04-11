6b4c8c42bbd989688034ea624914ee688520042b
Philipp Schwarzmuller <pschwarz@student.42heilbronn.de>
2023-04-11 10:58:53 +0200

    separate concerns of parsing

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -1,27 +1,50 @@
 #include "cub3D.h"
 
+static t_list	*read_file(int fd);
+static void		print_file(t_list *line_list);
+
 int	parse(int fd)
 {
-	char	*str;
-	t_list	*lines;
-	t_list	*head;
+	t_list	*line_list;
 
+	line_list = read_file(fd);
+	print_file(line_list);
+	close(fd);
+	return (EXIT_SUCCESS);
+}
+
+static void	print_file(t_list *line_list)
+{
+	t_list	*temp;
+
+	temp = line_list;
+	while (temp->next != NULL)
+	{
+		ft_printf("%s", temp->content);
+		temp = temp->next;
+	}
+	ft_printf("%s\n", temp->content);
+	ft_lstclear(&line_list, &free);
+}
+
+static t_list	*read_file(int fd)
+{
+	char	*str;
+	int		line_count;
+	t_list	*lines;
+
+	line_count = 0;
 	str = get_next_line(fd);
 	lines = NULL;
 	while (str != NULL)
 	{
 		if (str != NULL)
+		{
 			ft_lstadd_back(&lines, ft_lstnew(str));
+			line_count++;
+		}
 		str = get_next_line(fd);
 	}
-	head = lines;
-	while (lines->next != NULL)
-	{
-		ft_printf("%s", lines->content);
-		lines = lines->next;
-	}
-	ft_printf("%s\n", lines->content);
-	ft_lstclear(&head, &free);
-	close(fd);
-	return (EXIT_SUCCESS);
+	ft_printf("line count: %d\n", line_count);
+	return (lines);
 }
