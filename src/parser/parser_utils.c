b557369b2f59833d5e049d27d5dd30e73b150781
Philipp Schwarzmuller <pschwarz@student.42heilbronn.de>
2023-04-12 19:48:01 +0200

    add parsing of floor and ceiling colors

--- src/parser/parser_utils.c
+++ src/parser/parser_utils.c
@@ -51,25 +51,60 @@ int	get_rgba(int r, int g, int b, int a)
 	return (r << 24 | g << 16 | b << 8 | a);
 }
 
+void	parse_rgb(char *str, int *r, int *g, int *b)
+{
+	char	*temp;
+	char	**str_vals;
+	int		i;
+
+	temp = ft_strtrim(str, "FC ");
+	str_vals = ft_split(temp, ',');
+	i = 0;
+	while (i < 3)
+	{
+		if (i == 0)
+			*r = ft_atoi(str_vals[i]);
+		else if (i == 1)
+			*g = ft_atoi(str_vals[i]);
+		else if (i == 2)
+			*b = ft_atoi(str_vals[i]);
+		i++;
+	}
+	i = 0;
+	while (str_vals[i])
+	{
+		free(str_vals[i]);
+		i++;
+	}
+	free(str_vals);
+	free(temp);
+}
+
 void	parse_f_c(t_list *f_c_colors, t_map *map)
 {
 	char	*str;
+	int		r;
+	int		g;
+	int		b;
+	int		a;
 
-	(void) map;
+	a = 255;
 	while (f_c_colors->next != NULL)
 	{
 		str = (char *) f_c_colors->content;
+		parse_rgb(str, &r, &g, &b);
 		if (str[0] == 'F' && str[1] == ' ')
-			set_floor_color(map, get_rgba(250, 100, 0, 255));
+			set_floor_color(map, get_rgba(r, g, b, a));
 		else if (str[0] == 'C' && str[1] == ' ')
-			set_ceiling_color(map, get_rgba(255, 30, 0, 255));
+			set_ceiling_color(map, get_rgba(r, g, b, a));
 		f_c_colors = f_c_colors->next;
 	}
 	str = (char *) f_c_colors->content;
+	parse_rgb(str, &r, &g, &b);
 	if (str[0] == 'F' && str[1] == ' ')
-		set_floor_color(map, get_rgba(250, 100, 0, 255));
+		set_floor_color(map, get_rgba(r, g, b, a));
 	else if (str[0] == 'C' && str[1] == ' ')
-		set_ceiling_color(map, get_rgba(255, 30, 0, 255));
+		set_ceiling_color(map, get_rgba(r, g, b, a));
 }
 
 void	print_file(t_list *line_list)
