4b2c359af05ed86901f976ff1bba8893a169d41b
Florian Sandel <fsandel@student.42heilbronn.de>
2023-04-13 14:54:49 +0200

    changed movement from turn on ad to move left/right and added turn on q/e

--- src/keyboard_input.c
+++ src/keyboard_input.c
@@ -9,39 +9,48 @@ void	escape_handler(void *arg)
 		mlx_close_window(mlx);
 }
 
-static int	colission(t_window *window, char c, double speed)
+static int	colission(t_window *window, char c, double speed, t_vector *dir)
 {
 	t_vector	future;
 
 	if (c == 'x')
 	{
-		future.x = window->player->pos->x - window->player->dir->x * speed;
+		future.x = window->player->pos->x - dir->x * speed;
 		future.y = window->player->pos->y;
 	}
 	else
 	{
 		future.x = window->player->pos->x;
-		future.y = window->player->pos->y - window->player->dir->y * speed;
+		future.y = window->player->pos->y - dir->y * speed;
 	}
 	future.z = 0;
-	if (get_cube_type(&future, window->map) == walkable)
+	if (get_cube_type(&future, window->map) != wall)
 		return (0);
 	else
 		return (1);
 }
 
-static void	change_player_position(t_window *window, double speed)
+static void	change_player_position(t_window *window, double speed, double angle)
 {
-	if (!colission(window, 'x', speed))
+	t_vector	dir;
+	int			moved;
+
+	dir.x = window->player->dir->x;
+	dir.y = window->player->dir->y;
+	rotate_hor_f(&dir, &dir, angle);
+	moved = 0;
+	if (!colission(window, 'x', speed, &dir))
 	{
-		window->player->pos->x -= window->player->dir->x * speed;
-		draw_scene(window);
+		window->player->pos->x -= dir.x * speed;
+		moved = 1;
 	}
-	if (!colission(window, 'y', speed))
+	if (!colission(window, 'y', speed, &dir))
 	{
-		window->player->pos->y -= window->player->dir->y * speed;
-		draw_scene(window);
+		window->player->pos->y -= dir.y * speed;
+		moved = 1;
 	}
+	if (moved)
+		draw_scene(window);
 }
 
 void	player_movement(void *arg)
@@ -50,15 +59,21 @@ void	player_movement(void *arg)
 
 	window = (t_window *)arg;
 	if (mlx_is_key_down(window->mlx, MLX_KEY_W))
-		change_player_position(window, SPEED);
+		change_player_position(window, SPEED, 0);
 	if (mlx_is_key_down(window->mlx, MLX_KEY_S))
-		change_player_position(window, -SPEED);
+		change_player_position(window, SPEED, M_PI);
 	if (mlx_is_key_down(window->mlx, MLX_KEY_D))
+		change_player_position(window, SPEED, M_PI_2);
+	if (mlx_is_key_down(window->mlx, MLX_KEY_A))
+		change_player_position(window, SPEED, -M_PI_2);
+	if (mlx_is_key_down(window->mlx, MLX_KEY_RIGHT)
+		|| mlx_is_key_down(window->mlx, MLX_KEY_E))
 	{
 		rotate_hor_f(window->player->dir, window->player->dir, 0.1f);
 		draw_scene(window);
 	}
-	if (mlx_is_key_down(window->mlx, MLX_KEY_A))
+	if (mlx_is_key_down(window->mlx, MLX_KEY_LEFT)
+		|| mlx_is_key_down(window->mlx, MLX_KEY_Q))
 	{
 		rotate_hor_f(window->player->dir, window->player->dir, -0.1f);
 		draw_scene(window);
