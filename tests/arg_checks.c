9cae950e6eef3636c11f337f65c7be9a0f55a4aa
Philipp Schwarzmuller <pschwarz@student.42heilbronn.de>
2023-04-18 16:52:38 +0200

    make tests more pretty

---
+++ tests/arg_checks.c
@@ -0,0 +1,40 @@
+#include <criterion/criterion.h>
+#include <criterion/redirect.h>
+#include "../include/cub3D.h"
+#include "../lib/libft/libft.h"
+
+void	setup(void)
+{
+	cr_redirect_stderr();
+}
+
+TestSuite(valid_args, .init=setup);
+
+Test(valid_args, wrong_argc){
+char *test[3];
+test[0] = "cub3D";
+test[1] = "test";
+test[2] = "test";
+cr_expect_eq(args_valid(3, test), -1);
+}
+
+Test(valid_args, wrong_file_extension){
+char *test[2];
+test[0] = "cub3D";
+test[1] = "test";
+cr_expect_eq(args_valid(2, test), -1);
+}
+
+Test(valid_args, correct_args){
+char *test[2];
+test[0] = "cub3D";
+test[1] = "../maps/test.cub";
+cr_expect_gt(args_valid(2, test), 2);
+}
+
+Test(valid_args, non_existing_file){
+char *test[2];
+test[0] = "cub3D";
+test[1] = "../maps/test123123123123.cub";
+cr_expect_eq(args_valid(2, test), -1);
+}
