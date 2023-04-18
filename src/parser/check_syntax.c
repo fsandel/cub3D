5770836ce4f2ec9332f1d0bc870766a42bbcfbe9
Philipp Schwarzmuller <pschwarz@student.42heilbronn.de>
2023-04-18 08:02:32 +0200

    separate str validation from parsing

--- src/parser/check_syntax.c
+++ src/parser/check_syntax.c
@@ -1,6 +1,6 @@
 #include <cub3D.h>
 
-bool	is_map_char(char c, int *whitespace);
+static bool	is_map_char(char c, int *whitespace);
 
 bool	is_valid_map_str(char *map_str)
 {
@@ -21,7 +21,22 @@ bool	is_valid_map_str(char *map_str)
 	return (true);
 }
 
-bool	is_map_char(char c, int *whitespace)
+bool	is_valid_tex_str(char *tex_str)
+{
+	if (!ft_strncmp(tex_str, "NO", 2) || !ft_strncmp(tex_str, "SO", 2)
+		|| !ft_strncmp(tex_str, "WE", 2) || !ft_strncmp(tex_str, "EA", 2))
+		return (true);
+	return (false);
+}
+
+bool	is_valid_f_c_str(char *f_c_str)
+{
+	if (f_c_str[0] == 'F' || f_c_str[0] == 'C')
+		return (true);
+	return (false);
+}
+
+static bool	is_map_char(char c, int *whitespace)
 {
 	if (c == '1' || c == '0' || c == 'N' || c == 'E' || c == 'S' || c == 'W')
 		return (true);
