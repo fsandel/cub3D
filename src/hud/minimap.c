ed9e17c2ca71e29e4dd6f33e5eb2a8c6ff6f012f
Florian Sandel <fsandel@student.42heilbronn.de>
2023-04-19 15:44:28 +0200

    first working version of minimap

--- src/hud/minimap.c
+++ src/hud/minimap.c
@@ -10,19 +10,50 @@ bool is_on_screen(int offset_x, int offset_y, t_player *player, t_map *map)
 		return (false);
 }
 
+static bool is_on_map(double x, double y, t_map *map)
+{
+	if (x >= 0 && y >= 0 && x < map->width && y < map->height)
+		return (true);
+	else
+		return (false);
+}
+
 void	draw_minimap(t_window *window)
 {
-	int	x = -100;
-	int	y = -100;
-	
-	while (y < 101)
+	const int	size = 100;
+	int	x_iter;
+	int	y_iter;
+	const double	zoom = 3;
+	double	x_pos;
+	double	y_pos;
+	t_vector pos;
+
+	pos.z = 0;
+	y_iter = - size;
+	while (y_iter <= size)
 	{
-		x = -100;
-		while (x < 100)
+		x_iter = -size;
+		while (x_iter <= size)
 		{
-			mlx_put_pixel(window->hud->hud_img, window->hud->minimap->pix_pos_x + x, window->hud->minimap->pix_pos_y + y, WHITE);
-			x++;
+			if (sqrt(x_iter * x_iter + y_iter * y_iter) < 100)
+			{
+				x_pos = window->player->pos->x + x_iter * zoom / size;
+				y_pos = window->player->pos->y + y_iter * zoom / size;
+				set_vec(&pos, x_pos, y_pos, 0);
+				if (is_on_map(x_pos, y_pos, window->map))
+				{
+					if (get_cube_type(&pos, window->map) == wall)
+						mlx_put_pixel(window->hud->hud_img, window->hud->minimap->pix_pos_x + x_iter, window->hud->minimap->pix_pos_y + y_iter, WHITE);
+					else
+						mlx_put_pixel(window->hud->hud_img, window->hud->minimap->pix_pos_x + x_iter, window->hud->minimap->pix_pos_y + y_iter, BLUE);
+				}
+				else
+					mlx_put_pixel(window->hud->hud_img, window->hud->minimap->pix_pos_x + x_iter, window->hud->minimap->pix_pos_y + y_iter, RED);
+				if (abs(x_iter) < 3 && abs(y_iter) < 3)
+					mlx_put_pixel(window->hud->hud_img, window->hud->minimap->pix_pos_x + x_iter, window->hud->minimap->pix_pos_y + y_iter, PINK);
+			}
+			x_iter++;
 		}
-		y++;
+		y_iter++;
 	}
 }
