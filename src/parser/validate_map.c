d1f17ae67196f2c817b567a669b402330c5afbbc
Philipp Schwarzmuller <pschwarz@student.42heilbronn.de>
2023-04-19 19:06:18 +0200

    remove print statements and fix norm

--- src/parser/validate_map.c
+++ src/parser/validate_map.c
@@ -13,7 +13,6 @@ bool	map_is_valid(t_map *map)
 		column = 0;
 		while (column < map->width)
 		{
-			printf("at: %d x: %d y: %d\n", (int) map->cubes[line][column], column, line);
 			if (map->cubes[line][column] == walkable
 				&& !is_valid_field(map, line, column))
 				return (false);
@@ -26,17 +25,15 @@ bool	map_is_valid(t_map *map)
 
 bool	is_valid_field(t_map *map, int line, int column)
 {
-	printf("valid field? %d at x: %d y: %d\n",
-		(int) map->cubes[line][column], column, line);
 	if (line == 0 || line == map->height || column == 0 || column == map->width)
-		return (printf("first or last line/column\n"), false);
+		return (false);
 	if (map->cubes[line][column - 1] == empty)
-		return (printf("prior is illegal\n"), false);
+		return (false);
 	if (map->cubes[line][column + 1] == empty)
-		return (printf("next is illegal\n"), false);
+		return (false);
 	if (map->cubes[line - 1][column] == empty)
-		return (printf("upper is illegal\n"), false);
+		return (false);
 	if (map->cubes[line + 1][column] == empty)
-		return (printf("lower is illegal\n"), false);
+		return (false);
 	return (true);
 }
