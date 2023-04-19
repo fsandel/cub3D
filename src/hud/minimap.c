2b8a98f61d57b0530d4cf2b467b8d9c9abf64ebc
Florian Sandel <fsandel@student.42heilbronn.de>
2023-04-19 16:00:35 +0200

    added variable color and added ring around minimap

--- src/hud/minimap.c
+++ src/hud/minimap.c
@@ -20,13 +20,11 @@ static bool is_on_map(double x, double y, t_map *map)
 
 void	draw_minimap(t_window *window)
 {
-	const int	size = 100;
-	int	x_iter;
-	int	y_iter;
-	const double	zoom = 3;
-	double	x_pos;
-	double	y_pos;
-	t_vector pos;
+	const int		size = window->hud->minimap->radius;
+	const double	zoom = window->hud->minimap->zoom;
+	int				x_iter;
+	int				y_iter;
+	t_vector		pos;
 
 	pos.z = 0;
 	y_iter = - size;
@@ -35,23 +33,24 @@ void	draw_minimap(t_window *window)
 		x_iter = -size;
 		while (x_iter <= size)
 		{
-			if (sqrt(x_iter * x_iter + y_iter * y_iter) < 100)
+			if (sqrt(x_iter * x_iter + y_iter * y_iter) < size - 2)
 			{
-				x_pos = window->player->pos->x + x_iter * zoom / size;
-				y_pos = window->player->pos->y + y_iter * zoom / size;
-				set_vec(&pos, x_pos, y_pos, 0);
-				if (is_on_map(x_pos, y_pos, window->map))
+				pos.x = window->player->pos->x + x_iter * zoom / size;
+				pos.y = window->player->pos->y + y_iter * zoom / size;
+				if (is_on_map(pos.x, pos.y, window->map))
 				{
 					if (get_cube_type(&pos, window->map) == wall)
 						mlx_put_pixel(window->hud->hud_img, window->hud->minimap->pix_pos_x + x_iter, window->hud->minimap->pix_pos_y + y_iter, WHITE);
 					else
-						mlx_put_pixel(window->hud->hud_img, window->hud->minimap->pix_pos_x + x_iter, window->hud->minimap->pix_pos_y + y_iter, BLUE);
+						mlx_put_pixel(window->hud->hud_img, window->hud->minimap->pix_pos_x + x_iter, window->hud->minimap->pix_pos_y + y_iter, window->map->floor_color);
 				}
 				else
-					mlx_put_pixel(window->hud->hud_img, window->hud->minimap->pix_pos_x + x_iter, window->hud->minimap->pix_pos_y + y_iter, RED);
+					mlx_put_pixel(window->hud->hud_img, window->hud->minimap->pix_pos_x + x_iter, window->hud->minimap->pix_pos_y + y_iter, window->map->ceiling_color);
 				if (abs(x_iter) < 3 && abs(y_iter) < 3)
 					mlx_put_pixel(window->hud->hud_img, window->hud->minimap->pix_pos_x + x_iter, window->hud->minimap->pix_pos_y + y_iter, PINK);
 			}
+			else if (sqrt(x_iter * x_iter + y_iter * y_iter) < size)
+				mlx_put_pixel(window->hud->hud_img, window->hud->minimap->pix_pos_x + x_iter, window->hud->minimap->pix_pos_y + y_iter, BLACK);
 			x_iter++;
 		}
 		y_iter++;
