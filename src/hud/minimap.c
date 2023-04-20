66acfe5a1a976fb66deb1f68980c279edbd9395c
Florian Sandel <fsandel@student.42heilbronn.de>
2023-04-20 08:54:30 +0200

    added fov on minimap

--- src/hud/minimap.c
+++ src/hud/minimap.c
@@ -52,35 +52,42 @@ static void	put_minimap_circle(t_window *window, int x, int y)
 	else if (sqrt(x * x + y * y) < size)
 		put_pixel_minimap(x, y, window, BLACK);
 }
-static void	draw_minimap_ray(t_window *window, t_vector dir)
+
+static void	draw_minimap_ray(t_window *window, t_vector dir, t_minimap *minimap)
 {
 	t_vector	pos;
-	const int	size = window->hud->minimap->radius;
-	const int	zoom = window->hud->minimap->zoom;
+	const int	size = minimap->radius;
+	const int	zoom = minimap->zoom;
 	double		x;
 	double		y;
 
 	set_vec(&pos, window->player->pos->x, window->player->pos->y, 0);
 	while (get_cube_type(&pos, window->map) != wall)
 	{
-		x = (pos.x - window->player->pos->x) * size / zoom + window->hud->minimap->pix_pos_x;
-		y = (pos.y - window->player->pos->y) * size / zoom + window->hud->minimap->pix_pos_y;
-		if (x > 0 && y > 0 && x < WIDTH && y < HEIGHT && sqrt(pow(x - window->hud->minimap->pix_pos_x, 2)+ pow(y - window->hud->minimap->pix_pos_y, 2)) < size) 
-		mlx_put_pixel(window->hud->hud_img, x, y, 0x000000ff);
-		pos.x -= dir.x * 0.01;
-		pos.y -= dir.y * 0.01;
+		x = (pos.x - window->player->pos->x) * size / zoom + minimap->pix_pos_x;
+		y = (pos.y - window->player->pos->y) * size / zoom + minimap->pix_pos_y;
+		if (x > 0 && y > 0 && x < WIDTH && y < HEIGHT && sqrt(pow(x - minimap->pix_pos_x, 2)+ pow(y - minimap->pix_pos_y, 2)) < size - 2) 
+			mlx_put_pixel(window->hud->hud_img, x, y, 0xa0a0a0ee);
+		pos.x -= dir.x * 0.015;
+		pos.y -= dir.y * 0.015;
 	}
 }
+
 static void	draw_minimap_fov(t_window *window)
 {
 	t_vector	dir;
+	int iter;
+	const int rays = FOV;
 
-	set_vec(&dir, window->player->dir->x, window->player->dir->y, 0);
-	rotate_hor_f(&dir, &dir, FOV * M_PI / 180 / 2);
-	draw_minimap_ray(window, dir);
-	set_vec(&dir, window->player->dir->x, window->player->dir->y, 0);
-	rotate_hor_f(&dir, &dir, -FOV * M_PI / 180 / 2);
-	draw_minimap_ray(window, dir);
+	iter = -rays;
+	while (iter <= rays)
+	{
+		dir.x = window->player->dir->x;
+		dir.y = window->player->dir->y;
+		rotate_hor_f(&dir, &dir, FOV * M_PI * iter / rays / 360);
+		draw_minimap_ray(window, dir, window->hud->minimap);
+		iter ++;
+	}
 }
 
 void	draw_minimap(t_window *window)
