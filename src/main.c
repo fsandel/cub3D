03d0e70afc8cfcee5c4724336b2805b1b94af269
Florian Sandel <fsandel@student.42heilbronn.de>
2023-04-24 12:58:34 +0200

    fixed norm

--- src/main.c
+++ src/main.c
@@ -35,7 +35,8 @@ int	main(int argc, char **argv)
 	mlx_loop_hook(window->mlx, escape_handler, window->mlx);
 	mlx_loop_hook(window->mlx, player_movement, window);
 	mlx_loop_hook(window->mlx, draw_hud, window);
-	window->map->cubes[5][24] = door_closed;
+	mlx_key_hook(window->mlx, door_handler, window);
+	window->map->cubes[5][24] = door_open;
 	draw_scene(window);
 	mlx_loop(window->mlx);
 	free_window_struct(window);
--- src/math/misc.c
+++ src/math/misc.c
@@ -4,3 +4,8 @@ double	quad_add(double x, double y)
 {
 	return (sqrt(x * x + y * y));
 }
+
+double	ft_modf(double num)
+{
+	return (num - (int)num);
+}
