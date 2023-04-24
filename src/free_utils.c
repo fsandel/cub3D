0e686b2f00eede181c260b113689d2e909662b8b
Florian Sandel <fsandel@student.42heilbronn.de>
2023-04-24 13:24:27 +0200

    fixed leaks

--- src/free_utils.c
+++ src/free_utils.c
@@ -16,6 +16,7 @@ void	free_cubes(t_map *map)
 static void	free_hud(t_window *window)
 {
 	free(window->hud->fps);
+	free(window->hud->minimap);
 	free(window->hud);
 }
 
@@ -26,6 +27,8 @@ void	free_window_struct(t_window *window)
 	mlx_delete_texture(window->map->textures[east]);
 	mlx_delete_texture(window->map->textures[south]);
 	mlx_delete_texture(window->map->textures[west]);
+	mlx_delete_texture(window->map->door);
+	mlx_delete_texture(window->map->placeholder);
 	mlx_terminate(window->mlx);
 	free_cubes(window->map);
 	free_hud(window);
