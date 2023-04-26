086b579f661e0f2a6d83af1e1eedecf844bb1f6d
Florian Sandel <fsandel@student.42heilbronn.de>
2023-04-26 09:46:35 +0200

    fix norm

--- src/walls/raycasting.c
+++ src/walls/raycasting.c
@@ -100,7 +100,6 @@ static t_direction	cast_ray_dda(t_vector *pos, t_vector *dir,
 	double		dy;
 	double		angle;
 	t_vector	old;
-	//t_vector	test;
 
 	set_vec(target, pos->x, pos->y, pos->z);
 	while (get_cube_type(target, map) != wall
