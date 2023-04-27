16b2c720d005df604d4ad77e7a4682486c5c7b40
Florian Sandel <fsandel@student.42heilbronn.de>
2023-04-27 18:57:11 +0200

    added check for big fog

--- src/walls/fog.c
+++ src/walls/fog.c
@@ -40,6 +40,8 @@ static int	get_rgba_from_tex_fog(const mlx_texture_t *tex, int x, int y,
 	const int		pos = (y * tex->width + x) * tex->bytes_per_pixel;
 	const double	brightness = max(1.0 - (dis / FOG), 0);
 
+	if (dis > FOG)
+		return (0x000000ff);
 	color = get_rgba(tex->pixels[pos] * brightness,
 			tex->pixels[pos + 1] * brightness,
 			tex->pixels[pos + 2] * brightness,
@@ -56,6 +58,11 @@ static void	put_pixel_floor_fog(mlx_image_t *img, int pix_pos_x, int pix_pos_y,
 	const uint8_t	green = ((base_color >> 16) & 0xff) * brightness;
 	const uint8_t	blue = (base_color >> 24) * brightness;
 
-	base_color = (blue << 24) | (green << 16) | (red << 8) | alpha;
-	mlx_put_pixel(img, pix_pos_x, pix_pos_y, base_color);
+	if (brightness >= 1)
+		mlx_put_pixel(img, pix_pos_x, pix_pos_y, base_color);
+	else
+	{
+		base_color = (blue << 24) | (green << 16) | (red << 8) | alpha;
+		mlx_put_pixel(img, pix_pos_x, pix_pos_y, base_color);
+	}
 }
