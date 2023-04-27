2b8aedd45d8aebab001aa305f8a0e7fd49ef5da0
Florian Sandel <fsandel@student.42heilbronn.de>
2023-04-27 14:40:20 +0200

    removed some bloat calculation

--- src/main.c
+++ src/main.c
@@ -4,13 +4,13 @@ static t_window	*setup_window_struct(t_map *map)
 {
 	t_window	*window;
 
-	window = malloc(sizeof(t_window) * 1);
+	window = malloc(sizeof(t_window));
 	window->mlx = mlx_init(WIDTH, HEIGHT, "cub3D", 1);
 	window->img = mlx_new_image(window->mlx, WIDTH, HEIGHT);
 	mlx_image_to_window(window->mlx, window->img, 0, 0);
 	mlx_set_instance_depth(window->img->instances, 1);
 	window->map = map;
-	window->player = malloc(sizeof(t_player) * 1);
+	window->player = malloc(sizeof(t_player));
 	window->player->pos = window->map->start_pos;
 	window->player->dir = window->map->start_dir;
 	window->redraw = true;
