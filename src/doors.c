fa102e875bd82ff46037f4665c0f511298a397b2
Florian Sandel <fsandel@student.42heilbronn.de>
2023-04-27 14:04:57 +0200

    window now only redraws if door was actually opened

--- src/doors.c
+++ src/doors.c
@@ -1,13 +1,13 @@
 #include <cub3D.h>
 
-static void	toggle_targeted_door(t_window *window);
+static bool	toggle_targeted_door(t_window *window);
 
 void	door_handler(t_window *window, mlx_key_data_t keydata)
 {
 	if (keydata.key == MLX_KEY_SPACE && keydata.action == MLX_PRESS)
 	{
-		toggle_targeted_door(window);
-		draw_scene(window);
+		if (toggle_targeted_door(window))
+			draw_scene(window);
 	}
 }
 
@@ -27,18 +27,27 @@ static void	cast_iter_ray(t_window *window, t_vector dir, t_vector *target,
 	}
 }
 
-static void	toggle_targeted_door(t_window *window)
+static bool	toggle_targeted_door(t_window *window)
 {
 	t_vector	target;
 	double		dis;
+	bool		redraw;
 
+	redraw = false;
 	cast_iter_ray(window, *window->player->dir, &target, door_open);
 	dis = distance(*window->player->pos, target);
 	if (dis > COLLISION && dis < 2)
 	{
 		if (get_cube_type(&target, window->map) == door_closed)
+		{
 			window->map->cubes[(int)target.y][(int)target.x] = door_open;
+			redraw = true;
+		}
 		else if (get_cube_type(&target, window->map) == door_open)
+		{
 			window->map->cubes[(int)target.y][(int)target.x] = door_closed;
+			redraw = true;
+		}
 	}
+	return (redraw);
 }
