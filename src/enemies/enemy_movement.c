42697965aee5b3de03e7d24b6140c2693a3e70e0
Florian Sandel <fsandel@student.42heilbronn.de>
2023-04-28 18:46:16 +0200

    added movable enemies

--- src/enemies/enemy_movement.c
+++ src/enemies/enemy_movement.c
@@ -1,6 +1,36 @@
 #include <cub3D.h>
 
+static int	move_single_enemy(t_enemy *enemy, t_map *map)
+{
+	t_vector	future_pos;
+
+	if (enemy->state != hunting)
+		return (0);
+	future_pos.x = enemy->pos.x - enemy->dir.x * 0.01;
+	future_pos.y = enemy->pos.y - enemy->dir.y * 0.01;
+	if (get_cube_type(&future_pos, map) != wall
+		&& get_cube_type(&future_pos, map) != door_closed)
+	{
+		enemy->pos = future_pos;
+		return (1);
+	}
+	return (0);
+}
+
 void	move_enemies(t_window *window)
 {
-	(void)window;
+	int	i;
+	int	count_redraw;
+
+	i = 0;
+	count_redraw = 0;
+	while (window->all_enemies[i])
+	{
+		if (window->all_enemies[i]->state != dead)
+			count_redraw += move_single_enemy(window->all_enemies[i],
+					window->map);
+		i++;
+	}
+	if (count_redraw > 0)
+		window->redraw = 1;
 }
