5536b1cc9d90bcef5dabe0bc6df9c25e9d6fc934
fsandel <florian@sandel.email>
2023-04-29 13:39:09 +0200

    added function to sort enemies by distance

--- src/enemies/enemies.c
+++ src/enemies/enemies.c
@@ -1,5 +1,23 @@
 #include <cub3D.h>
 
+static void	sort_enemies(t_window *window)
+{
+	t_enemy	*temp;
+	int		i;
+
+	i = 0;
+	while (window->all_enemies[i] && window->all_enemies[i + 1])
+	{
+		if (window->all_enemies[i]->dis < window->all_enemies[i + 1]->dis)
+		{
+			temp = window->all_enemies[i];
+			window->all_enemies[i] = window->all_enemies[i + 1];
+			window->all_enemies[i + 1] = temp;
+		}
+		i++;
+	}
+}
+
 void	enemie_handler(void *arg)
 {
 	t_window	*window;
@@ -7,5 +25,6 @@ void	enemie_handler(void *arg)
 	window = (t_window *)arg;
 	check_enemies_state(window);
 	move_enemies(window);
+	sort_enemies(window);
 	draw_enemies(window);
 }
