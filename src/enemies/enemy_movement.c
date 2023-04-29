5aa7968367b34b6487d1a6e59493e5384dae2b01
fsandel <florian@sandel.email>
2023-04-29 20:27:58 +0200

    split up enemy movement into x and y

--- src/enemies/enemy_movement.c
+++ src/enemies/enemy_movement.c
@@ -1,20 +1,30 @@
 #include <cub3D.h>
 
-static int	move_single_enemy(t_enemy *enemy, t_map *map)
+static bool	move_single_enemy(t_enemy *enemy, t_map *map)
 {
 	t_vector	future_pos;
+	bool		moved;
 
+	moved = false;
 	if (enemy->state != hunting)
-		return (0);
+		return (false);
 	future_pos.x = enemy->pos.x - enemy->dir.x * 0.01;
+	future_pos.y = enemy->pos.y;
+	if (get_cube_type(&future_pos, map) != wall
+		&& get_cube_type(&future_pos, map) != door_closed)
+	{
+		enemy->pos = future_pos;
+		moved = true;
+	}
+	future_pos.x = enemy->pos.x;
 	future_pos.y = enemy->pos.y - enemy->dir.y * 0.01;
 	if (get_cube_type(&future_pos, map) != wall
 		&& get_cube_type(&future_pos, map) != door_closed)
 	{
 		enemy->pos = future_pos;
-		return (1);
+		moved = true;
 	}
-	return (0);
+	return (moved);
 }
 
 void	move_enemies(t_window *window)
