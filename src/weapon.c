0ebfa09f30e3079f25b66989440301fd5e2ba9fd
Florian Sandel <fsandel@student.42heilbronn.de>
2023-05-04 11:19:29 +0200

    added cleanup weapon and crosshair functions

--- src/weapon.c
+++ src/weapon.c
@@ -68,3 +68,49 @@ void	draw_cross_hair(t_window *window)
 					x_iter + WIDTH / 2, y_iter + HEIGHT / 2, CROSSHAIR_COLOR);
 	}
 }
+
+void	clean_weapon(t_window *window)
+{
+	int				x_iter;
+	int				y_iter;
+
+	y_iter = -WEAPON_SIZE_Y;
+	while (++y_iter < WEAPON_SIZE_Y)
+	{
+		if (y_iter + WEAPON_OFFSET_Y >= HEIGHT)
+			continue ;
+		x_iter = -WEAPON_SIZE_X;
+		while (++x_iter < WEAPON_SIZE_X)
+		{
+			if (x_iter + WEAPON_OFFSET_X >= WIDTH)
+				continue ;
+			mlx_put_pixel(window->hud->hud_img,
+				x_iter + WEAPON_OFFSET_X, y_iter + WEAPON_OFFSET_Y, 0x00000000);
+		}
+	}
+}
+
+void	clean_cross_hair(t_window *window)
+{
+	int			x_iter;
+	int			y_iter;
+	const int	size = 35;
+	const int	width = 0;
+
+	y_iter = -size - 1;
+	while (y_iter++ <= size)
+	{
+		x_iter = -width - 1;
+		while (x_iter++ <= width)
+			mlx_put_pixel(window->hud->hud_img,
+				x_iter + WIDTH / 2, y_iter + HEIGHT / 2, 0x00000000);
+	}
+	y_iter = -width - 1;
+	while (y_iter++ <= width)
+	{
+		x_iter = -size - 1;
+		while (x_iter++ <= size)
+			mlx_put_pixel(window->hud->hud_img,
+				x_iter + WIDTH / 2, y_iter + HEIGHT / 2, 0x00000000);
+	}
+}
