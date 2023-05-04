8658eb72beb59e4169f67c56d87da64c6bd8e7c4
Florian Sandel <fsandel@student.42heilbronn.de>
2023-05-04 11:16:13 +0200

    added weapon setup and free

--- src/free_utils.c
+++ src/free_utils.c
@@ -1,5 +1,7 @@
 #include <cub3D.h>
 
+static void	free_weapon(t_weapon *weapon);
+
 static void	free_all_enemies(t_window *window)
 {
 	int	i;
@@ -41,6 +43,19 @@ static void	free_hud(t_window *window)
 	free(window->hud);
 }
 
+static void	free_weapon(t_weapon *weapon)
+{
+	int	i;
+
+	i = 0;
+	while (weapon->gun_tex[i])
+		mlx_delete_texture(weapon->gun_tex[i++]);
+	i = 0;
+	while (weapon->torch_tex[i])
+		mlx_delete_texture(weapon->torch_tex[i++]);
+	free(weapon);
+}
+
 void	free_window_struct(t_window *window)
 {
 	free_all_enemies(window);
@@ -60,6 +75,7 @@ void	free_window_struct(t_window *window)
 	mlx_terminate(window->mlx);
 	free_cubes(window->map);
 	free_hud(window);
+	free_weapon(window->player->weapon);
 	free(window->player);
 	free(window->map->start_dir);
 	free(window->map->start_pos);
