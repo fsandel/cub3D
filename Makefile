31c9dbe4d7f2d3325cf264e2783616e0d9b54918
fsandel <florian@sandel.email>
2023-05-09 22:38:40 +0200

    added multiple enemies

--- Makefile
+++ Makefile
@@ -36,7 +36,7 @@ WALLS_FILES		=	raycasting.c textures.c fog.c
 
 ENEMIES			=	$(addprefix $(ENEMIES_DIR), $(ENEMIES_FILES))
 ENEMIES_DIR		=	src/enemies/
-ENEMIES_FILES	=	enemies.c enemy_draw.c enemy_movement.c enemy_state.c enemy_utils.c enemy_setup.c enemy_attack.c
+ENEMIES_FILES	=	enemies.c enemy_draw.c enemy_movement.c enemy_state.c enemy_utils.c enemy_setup.c enemy_attack.c enemy_load_png.c
 
 WEAPON			=	$(addprefix $(WEAPON_DIR), $(WEAPON_FILES))
 WEAPON_DIR		=	src/weapon/
--- src/enemies/enemy_setup.c
+++ src/enemies/enemy_setup.c
@@ -2,6 +2,7 @@
 
 static void	setup_enemy_textures(t_enemy **all_enemies);
 static void	setup_singular_enemy(t_enemy *enemy);
+void	enemy_load_png(t_enemy *enemy);
 
 t_enemy	**setup_enemy_struct(t_player *player, t_map *map)
 {
@@ -31,33 +32,13 @@ t_enemy	**setup_enemy_struct(t_player *player, t_map *map)
 
 static void	setup_singular_enemy(t_enemy *enemy)
 {
+	static	t_enemy_type	type = 0;
+
 	enemy->hitpoints = ENEMIE_HITPOINTS;
 	enemy->frame_cooldown = ENEMY_FRAME_COOLDOWN;
 	enemy->frame_count = 0;
 	enemy->state = out_of_range;
-	enemy->type = 0;
-}
-
-static bool	load_textures(t_enemy *enemy)
-{
-	enemy->tex_nb = 8;
-	enemy->walking_tex[0][0] = mlx_load_png("textures/enemy/big_hans_blue/m0.png");
-	enemy->walking_tex[0][1] = mlx_load_png("textures/enemy/big_hans_blue/m1.png");
-	enemy->walking_tex[0][2] = mlx_load_png("textures/enemy/big_hans_blue/m2.png");
-	enemy->walking_tex[0][3] = mlx_load_png("textures/enemy/big_hans_blue/m3.png");
-	enemy->walking_tex[0][4] = mlx_load_png("textures/enemy/big_hans_blue/m4.png");
-	enemy->walking_tex[0][5] = mlx_load_png("textures/enemy/big_hans_blue/m5.png");
-	enemy->walking_tex[0][6] = mlx_load_png("textures/enemy/big_hans_blue/m6.png");
-	enemy->walking_tex[0][7] = mlx_load_png("textures/enemy/big_hans_blue/m7.png");
-	enemy->attacking_tex[0][0] = mlx_load_png("textures/enemy/big_hans_blue/a0.png");
-	enemy->attacking_tex[0][1] = mlx_load_png("textures/enemy/big_hans_blue/a1.png");
-	enemy->attacking_tex[0][2] = mlx_load_png("textures/enemy/big_hans_blue/a2.png");
-	enemy->attacking_tex[0][3] = mlx_load_png("textures/enemy/big_hans_blue/a3.png");
-	enemy->attacking_tex[0][4] = mlx_load_png("textures/enemy/big_hans_blue/a4.png");
-	enemy->attacking_tex[0][5] = mlx_load_png("textures/enemy/big_hans_blue/a5.png");
-	enemy->attacking_tex[0][6] = mlx_load_png("textures/enemy/big_hans_blue/a6.png");
-	enemy->attacking_tex[0][7] = mlx_load_png("textures/enemy/big_hans_blue/a7.png");
-	return (true);
+	enemy->type = type++;
 }
 
 static void	setup_enemy_textures(t_enemy **all_enemies)
@@ -65,24 +46,24 @@ static void	setup_enemy_textures(t_enemy **all_enemies)
 	int		enemy_iter;
 	int		tex_iter;
 	t_enemy	tex_enemy;
-
+	t_enemy_type	type;
 	if (!all_enemies[0])
 		return ;
-	if (!load_textures(&tex_enemy))
-		return ;
+	enemy_load_png(&tex_enemy);
 	enemy_iter = 0;
 	while (all_enemies[enemy_iter])
 	{
 		tex_iter = 0;
-		while (tex_iter < tex_enemy.tex_nb)
+		while (tex_iter < 8)
 		{
-			all_enemies[enemy_iter]->walking_tex[0][tex_iter]
-				= tex_enemy.walking_tex[0][tex_iter];
-			all_enemies[enemy_iter]->attacking_tex[0][tex_iter]
-				= tex_enemy.attacking_tex[0][tex_iter];
+			type = all_enemies[enemy_iter]->type;
+			all_enemies[enemy_iter]->walking_tex[type][tex_iter]
+				= tex_enemy.walking_tex[type][tex_iter];
+			all_enemies[enemy_iter]->attacking_tex[type][tex_iter]
+				= tex_enemy.attacking_tex[type][tex_iter];
 			tex_iter++;
 		}
-		all_enemies[enemy_iter]->tex_nb = tex_enemy.tex_nb;
+		all_enemies[enemy_iter]->tex_nb = 8;
 		enemy_iter++;
 	}
 }
