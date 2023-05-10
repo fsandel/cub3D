c6b303fd0fd7df36ad6e21a22a5d7f0ecee18a45
Philipp Schwarzmuller <pschwarz@student.42heilbronn.de>
2023-05-10 15:02:32 +0200

    add valid texture testcases

--- tests/map_tests.c
+++ tests/map_tests.c
@@ -10,35 +10,6 @@ void	setup(void)
 
 TestSuite(parse_map, .init = setup);
 
-//Test(parse_map, valid_map){
-//	int			fd;
-//	t_map		*result;
-//	t_cube_type	expected_arr[8][10] = {
-//		{1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-//		{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-//		{1, 0, 0, 1, 1, 1, 1, 0, 0, 1},
-//		{1, 0, 0, 0, 1, 1, 0, 0, 0, 1},
-//		{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-//		{1, 0, 1, 0, 1, 0, 1, 0, 1, 1},
-//		{1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
-//		{1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
-//	};
-//	fd = open("../maps/test.cub", O_RDONLY);
-//	result = parse(fd);
-//	cr_assert_eq(result->width, 10);
-//	cr_assert_eq(result->height, 8);
-//	cr_assert_eq(result->floor_color, -878249985);
-//	cr_assert_eq(result->ceiling_color, 77981183);
-//	cr_assert_arr_eq(result->cubes[0], expected_arr[0], 10);
-//	cr_assert_arr_eq(result->cubes[1], expected_arr[1], 10);
-//	cr_assert_arr_eq(result->cubes[2], expected_arr[2], 10);
-//	cr_assert_arr_eq(result->cubes[3], expected_arr[3], 10);
-//	cr_assert_arr_eq(result->cubes[4], expected_arr[4], 10);
-//	cr_assert_arr_eq(result->cubes[5], expected_arr[5], 10);
-//	cr_assert_arr_eq(result->cubes[6], expected_arr[6], 10);
-//	cr_assert_arr_eq(result->cubes[7], expected_arr[7], 10);
-//}
-
 Test(parse_map, invalid_area){
 	int i;
 	char	*map_name;
@@ -203,44 +174,85 @@ Test(parse_map, valid_rgb){
 		i++;
 	}
 }
-//
-//Test(parse_map, invalid_tex_NO){
-//	int i;
-//	char	*map_name;
-//	char	*num_str;
-//	char	*temp;
-//	int		fd;
-//	t_map	*result;
-//	
-//	i = 0;
-//	while (i < 23)
-//	{
-//		if (i < 10)
-//		{
-//			num_str = ft_itoa(i);
-//			temp = ft_strjoin("invalid_maps/invalid_tex_NO_00", num_str);
-//			free(num_str);
-//			map_name = ft_strjoin(temp, ".cub");
-//			free(temp);
-//			fd = open(map_name, O_RDONLY);
-//			result = parse(fd);
-//			cr_expect(options_are_valid(result) == false, "failed on %s \n", map_name);
-//			free(map_name);
-//			close(fd);
-//		}
-//		else
-//		{
-//			num_str = ft_itoa(i);
-//			temp = ft_strjoin("invalid_maps/invalid_tex_NO_0", num_str);
-//			free(num_str);
-//			map_name = ft_strjoin(temp, ".cub");
-//			free(temp);
-//			fd = open(map_name, O_RDONLY);
-//			result = parse(fd);
-//			cr_expect(options_are_valid(result) == false, "failed on %s \n", map_name);
-//			free(map_name);
-//			close(fd);
-//		}
-//		i++;
-//	}
-//}
+
+Test(parse_map, invalid_tex_NO){
+	int i;
+	char	*map_name;
+	char	*num_str;
+	char	*temp;
+	int		fd;
+	t_map	*result;
+	
+	i = 0;
+	while (i < 23)
+	{
+		if (i < 10)
+		{
+			num_str = ft_itoa(i);
+			temp = ft_strjoin("invalid_maps/invalid_tex_NO_00", num_str);
+			free(num_str);
+			map_name = ft_strjoin(temp, ".cub");
+			free(temp);
+			fd = open(map_name, O_RDONLY);
+			result = parse(fd);
+			cr_expect(result == NULL, "failed on %s \n", map_name);
+			free(map_name);
+			close(fd);
+		}
+		else
+		{
+			num_str = ft_itoa(i);
+			temp = ft_strjoin("invalid_maps/invalid_tex_NO_0", num_str);
+			free(num_str);
+			map_name = ft_strjoin(temp, ".cub");
+			free(temp);
+			fd = open(map_name, O_RDONLY);
+			result = parse(fd);
+			cr_expect(result == NULL, "failed on %s \n", map_name);
+			free(map_name);
+			close(fd);
+		}
+		i++;
+	}
+}
+
+Test(parse_map, valid_tex){
+	int i;
+	char	*map_name;
+	char	*num_str;
+	char	*temp;
+	int		fd;
+	t_map	*result;
+	
+	i = 0;
+	while (i < 50)
+	{
+		if (i < 10)
+		{
+			num_str = ft_itoa(i);
+			temp = ft_strjoin("valid_maps/valid_tex_00", num_str);
+			free(num_str);
+			map_name = ft_strjoin(temp, ".cub");
+			free(temp);
+			fd = open(map_name, O_RDONLY);
+			result = parse(fd);
+			cr_expect(result != NULL, "failed on %s \n", map_name);
+			free(map_name);
+			close(fd);
+		}
+		else
+		{
+			num_str = ft_itoa(i);
+			temp = ft_strjoin("valid_maps/valid_tex_0", num_str);
+			free(num_str);
+			map_name = ft_strjoin(temp, ".cub");
+			free(temp);
+			fd = open(map_name, O_RDONLY);
+			result = parse(fd);
+			cr_expect(result != NULL, "failed on %s \n", map_name);
+			free(map_name);
+			close(fd);
+		}
+		i++;
+	}
+}
