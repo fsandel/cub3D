ec5f7fbb14cd935deea3df7509da90260731b27a
Philipp <51891575+philippschwarzmueller@users.noreply.github.com>
2023-05-10 18:56:54 +0200

    make proper use of ft_mlx_delete_texture

--- src/free/free_utils2.c
+++ src/free/free_utils2.c
@@ -14,17 +14,12 @@ void	free_filecontent(t_file_content *file_content)
 
 void	free_map(t_map *map)
 {
-	if (map->textures[north])
-		ft_mlx_delete_texture(map->textures[north]);
-	if (map->textures[east])
-		ft_mlx_delete_texture(map->textures[east]);
-	if (map->textures[south])
-		ft_mlx_delete_texture(map->textures[south]);
-	if (map->textures[west])
-		ft_mlx_delete_texture(map->textures[west]);
+	ft_mlx_delete_texture(map->textures[north]);
+	ft_mlx_delete_texture(map->textures[east]);
+	ft_mlx_delete_texture(map->textures[south]);
+	ft_mlx_delete_texture(map->textures[west]);
 	free(map->state);
-	if (map->door_tex)
-		ft_mlx_delete_texture(map->door_tex);
+	ft_mlx_delete_texture(map->door_tex);
 	if (map->cubes)
 		free_cubes(map);
 	free(map);
